143bfd135eb7f6501a91c9e80c0978c6d16e4331
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 10:39:23 +0100

    valid

--- src/main/java/com/example/demo/bean/Session.java
+++ src/main/java/com/example/demo/bean/Session.java
@@ -1,5 +1,33 @@
 package com.example.demo.bean;
 
 public class Session {
-
+	private int id,duree;
+	private String date,titre;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDuree() {
+		return duree;
+	}
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	
+	
 }
--- src/main/java/com/example/demo/validation/Validator.java
+++ src/main/java/com/example/demo/validation/Validator.java
@@ -1,5 +1,13 @@
 package com.example.demo.validation;
 
 public class Validator {
+public boolean validPrix(float prix)
+{
+return 	prix>100;
 
+}
+public boolean validDescription(String description)
+{
+return description.length()>10;	
+}
 }
