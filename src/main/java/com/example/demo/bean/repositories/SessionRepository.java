f6bcf81e39b494af6788912bfbf2af156346f516
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 14:26:30 +0100

    Create SessionRepository.java

---
+++ src/main/java/com/example/demo/bean/repositories/SessionRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.bean.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.bean.Session;
+
+public interface SessionRepository extends CrudRepository<Session, Integer> {
+
+}
