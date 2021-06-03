8f276144767ff67f4e67bfad298d18d6296e88e3
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 10:47:50 +0100

    Update ApplicationTest.java

--- src/test/java/com/example/demo/ApplicationTest.java
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
-public class ApplicationTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.validation.Validator;
+
+public class ApplicationTest {
+Validator v;
+	@BeforeEach
+void initialize()
+{
+		v=new Validator();
+}
+	@Test
+	void testTitre()
+	{
+		assertTrue(v.testNotBlank("hello"));
+		
+	}
 }
