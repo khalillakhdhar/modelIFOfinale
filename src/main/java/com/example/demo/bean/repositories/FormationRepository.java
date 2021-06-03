6e475d5c762667ca1302e5dfbc125e8d1e4053a8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 14:25:42 +0100

    Create FormationRepository.java

---
+++ src/main/java/com/example/demo/bean/repositories/FormationRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.bean.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.bean.Formation;
+
+public interface FormationRepository extends CrudRepository<Formation, Integer> {
+
+}
