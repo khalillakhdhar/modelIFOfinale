2d8b3cadeb6c950a9953a1bb3980b29de6086f25
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 14:34:44 +0100

    controllers

--- src/main/java/com/example/demo/Controller/FormationController.java
+++ src/main/java/com/example/demo/Controller/FormationController.java
@@ -1,5 +1,33 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.Formation;
+import com.example.demo.bean.repositories.FormationRepository;
+
+@RestController
 public class FormationController {
+@Autowired
+FormationRepository formationrepository;
+@GetMapping("formation")
+public List<Formation> getallformations()
+{
+	return (List<Formation>) formationrepository.findAll();
+}
+@PostMapping("formation")
+public Formation addFormation(@Valid @RequestBody Formation  formation)
+{
+return formationrepository.save(formation);	
+}
+
+
 
 }
--- src/main/java/com/example/demo/Controller/SessionController.java
+++ src/main/java/com/example/demo/Controller/SessionController.java
@@ -1,5 +1,16 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.repositories.FormationRepository;
+
+@RestController
+@RequestMapping("session")
 public class SessionController {
+@Autowired
+FormationRepository formationRepository;
 
 }
