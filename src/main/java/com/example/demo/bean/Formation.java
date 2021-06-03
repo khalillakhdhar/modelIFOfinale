8e2fe48b1bdd3a224d54148f308ad8ec774c28ea
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 10:36:00 +0100

    Update Formation.java

--- src/main/java/com/example/demo/bean/Formation.java
+++ src/main/java/com/example/demo/bean/Formation.java
@@ -1,5 +1,34 @@
 package com.example.demo.bean;
 
 public class Formation {
+private int id;
+private float prix;
+private String description,titre;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public float getPrix() {
+	return prix;
+}
+public void setPrix(float prix) {
+	this.prix = prix;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+
 
 }
